88b9fbb3966204cde0b2d7d9b7434e20de5b5d79
HARUKA <haruka.music.style@icloud.com>
2025-01-20 18:49:15 +0900

    css

--- website/src/app/pages/LoginPage/login.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "@/app/pages/LoginPage/login.module.css";
-
-const login = () => {
-  return <div className="classes.container"></div>;
-};
-
-export default login;
