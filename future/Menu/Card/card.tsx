ed6a0f22748bc44edc47026142bd1def075d510b
HARUKA <haruka.music.style@icloud.com>
2025-01-31 15:51:11 +0900

    card

---
+++ future/Menu/Card/card.tsx
@@ -0,0 +1,185 @@
+import React from "react";
+import Image from "next/image";
+import classes from "../Card/card.module.css";
+import eggs from "../../../public/eggs.jpg";
+import pizza from "../../../public//pizza.jpg";
+import wine from "../../../public//wine.jpg";
+import cake from "../../../public//cake.jpg";
+
+const card = () => {
+  return (
+    <>
+      <section className={classes.section}>
+        <ul className={classes.Grid}>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <span className={classes.label}>New</span>
+              <Image
+                className={classes.image}
+                src={eggs}
+                alt="eggs"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <span className={classes.label}>New</span>
+              <Image
+                className={classes.image}
+                src={pizza}
+                alt="pizza"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <Image
+                className={classes.image}
+                src={wine}
+                alt="wine"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <Image
+                className={classes.image}
+                src={cake}
+                alt="cake"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <Image
+                className={classes.image}
+                src={cake}
+                alt="cake"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <span className={classes.label}>New</span>
+              <Image
+                className={classes.image}
+                src={eggs}
+                alt="eggs"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <span className={classes.label}>New</span>
+              <Image
+                className={classes.image}
+                src={pizza}
+                alt="pizza"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default card;
--- src/app/pages/MenuPage/menu.tsx
+++ src/app/pages/MenuPage/menu.tsx
@@ -1,44 +1,26 @@
 import React from "react";
-import Image from "next/image";
 import classes from "@/app/pages/MenuPage/menu.module.css";
+import Card from "../../../../future/Menu/Card/card";
 
 const menu = () => {
   return (
     <>
       <strong className={classes.strong}>
-        Our Menu
-        <p>
+        <p className={classes.SpHeadline}>Our Menu</p>
+        <p className={classes.Spdescription}>
           We consider all the drivers of change gives you the components
           <br />
           you need to chenge to create a truly happens.
         </p>
       </strong>
-      <button className={classes.button}>All</button>
-      <button className={classes.button}>Breakfast</button>
-      <button className={classes.button}>Main Dishes</button>
-      <button className={classes.button}>Drinks</button>
-      <button className={classes.button}>Dessorts</button>
-      <section className={classes.section}>
-        <ul className={classes.Grid}>
-          <li className={classes.GridItem}>
-            <article className={classes.Card}>
-              <a href="#" className={classes.CardLink}>
-                <span className={classes.label}>New</span>
-                <image></image>
-                <div className={classes.info}>
-                  <h1 className={classes.price}>$9.99</h1>
-                  <h2 className={classes.headline}>Fried Egge</h2>
-                  <p className={classes.description}>
-                    Made with eggs. lettuce,salt,oil
-                    <br />
-                    and other ingredients.
-                  </p>
-                </div>
-              </a>
-            </article>
-          </li>
-        </ul>
-      </section>
+      <nav className={classes.ButtonNav}>
+        <button className={classes.button}>All</button>
+        <button className={classes.button}>Breakfast</button>
+        <button className={classes.button}>Main Dishes</button>
+        <button className={classes.button}>Drinks</button>
+        <button className={classes.button}>Dessorts</button>
+      </nav>
+      <Card />
     </>
   );
 };
