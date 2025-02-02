94d963164f0a51a00e02d5c16c7e948df24f9888
HARUKA <haruka.music.style@icloud.com>
2025-02-02 12:22:51 +0900

    card

--- future/Menu/Card/card.tsx
+++ future/Menu/Card/card.tsx
@@ -5,6 +5,10 @@ import eggs from "../../../public/eggs.jpg";
 import pizza from "../../../public//pizza.jpg";
 import wine from "../../../public//wine.jpg";
 import cake from "../../../public//cake.jpg";
+import juice from "../../../public/juice.jpg";
+import ice from "../../../public/ice.jpg";
+import hamburger from "../../../public/hamburger.jpg";
+import waffle from "../../../public/waffle.jpg";
 
 const card = () => {
   return (
@@ -109,7 +113,7 @@ const card = () => {
             <article className={classes.Card}>
               <Image
                 className={classes.image}
-                src={cake}
+                src={juice}
                 alt="cake"
                 width={200}
                 height={200}
@@ -133,7 +137,7 @@ const card = () => {
               <span className={classes.label}>New</span>
               <Image
                 className={classes.image}
-                src={eggs}
+                src={ice}
                 alt="eggs"
                 width={200}
                 height={200}
@@ -157,7 +161,31 @@ const card = () => {
               <span className={classes.label}>New</span>
               <Image
                 className={classes.image}
-                src={pizza}
+                src={hamburger}
+                alt="pizza"
+                width={200}
+                height={200}
+              />
+              <div className={classes.info}>
+                <h1 className={classes.price}>$9.99</h1>
+                <h2 className={classes.headline}>Fried Egge</h2>
+                <p className={classes.description}>
+                  Made with eggs. lettuce,salt,oil
+                  <br />
+                  and other ingredients.
+                </p>
+              </div>
+              <a href="#" className={classes.CardLink}>
+                <button>submit</button>
+              </a>
+            </article>
+          </li>
+          <li className={classes.GridItem}>
+            <article className={classes.Card}>
+              <span className={classes.label}>New</span>
+              <Image
+                className={classes.image}
+                src={waffle}
                 alt="pizza"
                 width={200}
                 height={200}
